5e78fd73362b7b093eb6ac73cf28c12b51ffa7bd
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-04-01 21:04:03 +0530

    str

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
     <div id="main">
-
+      <image />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
